63a724dae382200fa0aa587c800adc8038127dd8
Laura Messa Travin <laura.travin@gmail.com>
2019-05-10 03:38:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,8 +45,8 @@ class ApplicationController < Sinatra::Base
       #  binding.pry
        @mod= Recipe.find(params[:id])
        @mod.update(params[:recipe])
-
-     redirect "/recipes/#{@mod.id}"  #esta forma se llama interpolation
+       var = @mod.id
+     redirect "/recipes/#{var}"  #esta forma se llama interpolation
   end
 
 
