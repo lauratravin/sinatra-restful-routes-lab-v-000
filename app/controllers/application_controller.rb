1a74644419b939e0583b0226506c867a94ea2e87
Laura Messa Travin <laura.travin@gmail.com>
2019-05-10 03:49:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
   #new post created
   post '/recipes' do
     binding.pry
-    @new_recipe= Recipe.create(name: params[name], ingredients: params[ingredients], cook_time: params[cook_time])
+    @new_recipe= Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
 
     redirect to '/recipes/'+ @new_recipe.id
 #      redirect "/recipes/#{@new_recipe.id}"
